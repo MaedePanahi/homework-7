b28262230b0abb00125e6646c37f37e38d63af72
MaedePanahi <maepnh2020@gmail.com>
2023-01-24 23:58:34 +0330

    create forecast

--- src/index.js
+++ src/index.js
@@ -14,7 +14,56 @@ let currentDate = document.querySelector("#date");
 
 currentDate.innerHTML = `Last update: ${day} ${date}, ${hours}:${minutes}`;
 
-
+function formatDay(timestamp) {
+    let date = new Date(timestamp * 1000);
+    let day = date.getDay();
+    let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  
+    return days[day];
+  }
+function displayForecast(response) {
+    let forecast = response.data.daily;
+  
+    let forecastElement = document.querySelector("#forecast");
+  
+    let forecastHTML = `<div class="row">`;
+    forecast.forEach(function (forecastDay, index) {
+      if (index < 6) {
+        forecastHTML =
+          forecastHTML +
+          `
+        <div class="col-2">
+          <div class="weather-forecast-date">${formatDay(response.data.daily)}</div>
+          <img
+            src="http://openweathermap.org/img/wn/${
+              response.data.daily.icon
+            }@2x.png"
+            alt=""
+            width="42"
+          />
+          <div class="weather-forecast-temperatures">
+            <span class="weather-forecast-temperature-max"> ${Math.round(
+              response.data.daily.temperature.maximum
+            )}° </span>
+            <span class="weather-forecast-temperature-min"> ${Math.round(
+                response.data.daily.temperature.minimum
+            )}° </span>
+          </div>
+        </div>
+    `;
+      }
+    });
+  
+    forecastHTML = forecastHTML + `</div>`;
+    forecastElement.innerHTML = forecastHTML;
+  }
+  
+  function getForecast(coordinates) {
+   
+    let apiUrl = `https://api.shecodes.io/weather/v1/forecast?lat=${coordinates.lat}&lon=${coordinates.lon}&key=b69109c34o2t8f93bad7f025530244c3&units=metric`;
+    
+    axios.get(apiUrl).then(displayForecast);
+  }
 
 function ShowWeather(response){
   console.log(response.data)
@@ -29,6 +78,7 @@ function ShowWeather(response){
   Humidity.innerHTML=`Humidity : ${Math.round(response.data.temperature.humidity)}`;
   Wind.innerHTML=`Wind : ${Math.round(response.data.wind.speed)}`;
   IconElement.setAttribute("src", `http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${response.data.condition.icon}.png`);
+  getForecast(response.data.coord);
 }
 
 function getWeather(query){
@@ -36,11 +86,11 @@ let url=`https://api.shecodes.io/weather/v1/current?query=${query}&key=b69109c34
 
 axios.get(url).then(ShowWeather);
 }
-function clearThis(target) {
-    if (target.value == 'exemple') {
-        target.value = "";
-    }
-}
+// function clearThis(target) {
+//     if (target.value == 'exemple') {
+//         target.value = "";
+//     }
+// }
 function Search(event){
     event.preventDefault();
     let Input = document.querySelector("#form-control");
